23874481d7548643bbfb1a1523557307b61fcc4a
Caball009 <82909616+Caball009@users.noreply.github.com>
2022-09-27 20:50:45 +0200

    Temporary fix for timescale slider
    Timescale slider doesn't accept text input anymore. The value of timescale is adjusted to closest accepted value, using the lower bound.

--- core/src/UI/Components/ControlBar.cpp
+++ core/src/UI/Components/ControlBar.cpp
@@ -55,7 +55,17 @@ namespace IWXMVM::UI
 
 		ImGui::SameLine(playbackSpeedSliderX);
 		ImGui::SetNextItemWidth(playbackSpeedSliderWidth);
-		ImGui::SliderFloat("##", (float*)timescale.value().value, 0.001f, 1000.0f, "%.3f", ImGuiSliderFlags_Logarithmic);
+		ImGui::SliderFloat("##", &timescale.value().value->floating_point, 0.001f, 1000.0f, "%.3f", ImGuiSliderFlags_Logarithmic | ImGuiSliderFlags_NoInput);
+
+		static constexpr std::array timescales{ 0.001, 0.002, 0.005, 0.0075, 0.01, 0.02, 0.025, 0.05, 0.075, 0.1, 0.125, 0.2, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0, 500.0, 1000.0 };
+		for (std::size_t i = 1; i < timescales.size(); ++i) 
+		{
+			if (timescale.value().value->floating_point > timescales[i - 1] + 0.0000001 && timescale.value().value->floating_point < timescales[i] - 0.0000001)
+			{
+				timescale.value().value->floating_point = timescales[i - 1];
+				break;
+			}
+		}
 
 		auto pauseButtonSize = ImVec2(panelSize.y / 3.6f, panelSize.y / 3.6f);
 
