6333403b52b5c4e6539fce419c52bd813872ff9b
reallyluckyy <reallyluckyy@users.noreply.github.com>
2023-08-05 22:37:45 +0200

    add FX_SetupCamera hook

--- iw3/src/Hooks/Camera.cpp
+++ iw3/src/Hooks/Camera.cpp
@@ -63,7 +63,7 @@ namespace IWXMVM::IW3::Hooks::Camera
 		angles[2] = camera.GetRotation()[2];
 	}
 
-	uint32_t AnglesToAxis_Address = 0x5635C0;
+	uintptr_t AnglesToAxis_Address = 0x5635C0;
 	void __declspec(naked) AnglesToAxis_Hook()
 	{
 		static float* angles;
@@ -77,6 +77,30 @@ namespace IWXMVM::IW3::Hooks::Camera
 		__asm jmp AnglesToAxis_Address
 	}
 
+	void FX_SetupCamera() 
+	{
+		auto& camera = Mod::GetCameraManager()->GetActiveCamera();
+
+		if (!camera.IsModControlledCameraMode())
+			return;
+
+		auto refdef = Structures::GetRefDef();
+		refdef->Origin[0] = camera.GetPosition()[0];
+		refdef->Origin[1] = camera.GetPosition()[1];
+		refdef->Origin[2] = camera.GetPosition()[2];
+	}
+
+	uintptr_t FX_SetupCamera_Trampoline;
+	void __declspec(naked) FX_SetupCamera_Hook()
+	{
+		__asm pushad
+
+		FX_SetupCamera();
+
+		__asm popad
+		__asm jmp FX_SetupCamera_Trampoline
+	}
+
 	void Install()
 	{
 		// rewrite the camera position and fov
@@ -86,7 +110,9 @@ namespace IWXMVM::IW3::Hooks::Camera
 		HookManager::WriteCall(0x451C8D, (uintptr_t)AnglesToAxis_Hook);
 		HookManager::WriteCall(0x44FD8C, (uintptr_t)AnglesToAxis_Hook);
 
-		// TODO: FX_SetupCamera
+		// update position of world-space effects (such as smoke) with our new position
+		HookManager::CreateHook(0x4A56F0, (uintptr_t)FX_SetupCamera_Hook, &FX_SetupCamera_Trampoline);
+		
 		// TODO: CG_CalcFov
 	}
 }
