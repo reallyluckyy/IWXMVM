dde2aab146022a2f47fa7af429d6cbeb47293a9b
Caball009 <82909616+Caball009@users.noreply.github.com>
2023-08-17 21:21:29 +0200

    Unused function declaration

--- core/src/UI/Components/CustomImGuiControls.hpp
+++ core/src/UI/Components/CustomImGuiControls.hpp
@@ -4,5 +4,4 @@ namespace ImGui
 {
 	bool DemoProgressBar(uint32_t* currentTick, const uint32_t endTick, const ImVec2& size_arg, const char* overlay);
 	bool TimescaleSlider(const char* label, float* v, float v_min, float v_max, const char* format, ImGuiSliderFlags flags);
-	void DemoFileTree(const std::vector<std::filesystem::path>& searchPaths, const std::set<std::filesystem::path>& subDirectories, const std::vector<std::filesystem::path>& demoPaths, std::function<void(const std::filesystem::path&)> renderFileDisplay);
 }
