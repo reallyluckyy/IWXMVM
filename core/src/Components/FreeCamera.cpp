9c5b2d79831bdde294460f26b7ed8e4481304a43
reallyluckyy <reallyluckyy@users.noreply.github.com>
2023-12-28 02:24:17 +0100

    Displaycurrent fov and camera roll when changed

--- core/src/Components/FreeCamera.cpp
+++ core/src/Components/FreeCamera.cpp
@@ -2,12 +2,54 @@
 #include "FreeCamera.hpp"
 
 #include "Input.hpp"
+#include "Events.hpp"
+#include "Resources.hpp"
 #include "UI/UIManager.hpp"
+#include "Components/CameraManager.hpp"
 
 namespace IWXMVM::Components
 {
+    constexpr float PROPERTY_DISPLAY_TIME = 4.0f;
+    float fovDisplayTimer = 0.0f;
+    float rotationDisplayTimer = 0.0f;
+
+    int GetAlphaForTimer(float timer)
+    {
+        return (int)(timer / PROPERTY_DISPLAY_TIME * 2 * 255.0f);
+    }
+
     void FreeCamera::Initialize()
     {
+        Events::RegisterListener(EventType::OnRenderGameView, [&]() {
+
+            if (CameraManager::Get().GetActiveCamera()->GetMode() != Camera::Mode::Free)
+                return;
+
+            auto& gameView = UI::UIManager::Get().GetUIComponent(UI::Component::GameView);
+
+            if (fovDisplayTimer > 0)
+            {
+                auto label = std::format(ICON_FA_VIDEO " {0:.0f} ", this->GetFov());
+                auto pos = gameView->GetPosition() + gameView->GetSize() -
+                           ImVec2(ImGui::GetFontSize() * 5, ImGui::GetFontSize() * 2) -
+                           ImGui::CalcTextSize(label.c_str());
+                ImGui::GetWindowDrawList()->AddText(pos,
+                                                    ImColor(255, 255, 255, GetAlphaForTimer(fovDisplayTimer)),
+                                                    label.c_str());
+                fovDisplayTimer -= Input::GetDeltaTime();
+            }
+
+            if (rotationDisplayTimer > 0)
+            {
+                auto label = std::format(ICON_FA_ARROW_ROTATE_RIGHT " {0:.0f} ", this->GetRotation()[2]);
+                auto pos = gameView->GetPosition() +
+                           ImVec2(ImGui::GetFontSize() * 5, gameView->GetSize().y - ImGui::GetFontSize() * 2) - 
+                           ImVec2(0, ImGui::CalcTextSize(label.c_str()).y);
+                ImGui::GetWindowDrawList()->AddText(pos, ImColor(255, 255, 255, GetAlphaForTimer(rotationDisplayTimer)),
+                                                    label.c_str());
+                rotationDisplayTimer -= Input::GetDeltaTime();
+            }
+        });
     }
 
     void FreeCamera::Update()
@@ -56,6 +98,7 @@ namespace IWXMVM::Components
         if (Input::KeyHeld(ImGuiKey_LeftAlt))
         {
             this->GetRotation()[2] += Input::GetScrollDelta();
+            rotationDisplayTimer = PROPERTY_DISPLAY_TIME;
         }
         else
         {
@@ -66,6 +109,7 @@ namespace IWXMVM::Components
                 this->GetFov() += scrollDelta * Input::GetDeltaTime() * 32.0f;
                 this->GetFov() = glm::clamp(this->GetFov(), 1.0f, 179.0f);
                 scrollDelta *= SMOOTHING_FACTOR;
+                fovDisplayTimer = PROPERTY_DISPLAY_TIME;
             }
             else if (scrollDelta != 0.0)
             {
