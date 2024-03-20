777e5c697082b6a164650e3d9906e24fb30fc289
reallyluckyy <reallyluckyy@users.noreply.github.com>
2024-03-20 21:51:11 +0100

    Fix indentation

--- core/src/UI/Components/PlayerAnimationUI.cpp
+++ core/src/UI/Components/PlayerAnimationUI.cpp
@@ -9,69 +9,69 @@
 
 namespace IWXMVM::UI
 {
-bool DrawHeaderAndResetButton(const char* label)
-{
-    ImGui::AlignTextToFramePadding();
-    ImGui::PushFont(UIManager::Get().GetBoldFont());
-    ImGui::Text(label);
-    ImGui::PopFont();
-    ImGui::SameLine();
-
-    const char* resetlabel = ICON_FA_REPEAT " Reset ";
-    const bool ret = ImGui::Button(resetlabel);
-    return ret;
-}
-
-void PlayerAnimation::Render()
-{
-    if (!visible)
-        return;
-
-    ImGui::SetNextWindowFocus();
-
-    if (Mod::GetGameInterface()->GetGameState() != Types::GameState::InDemo)
+    bool DrawHeaderAndResetButton(const char* label)
     {
-        ImGui::Begin("Player Death Animations##1", &visible, ImGuiWindowFlags_NoCollapse);
-        UI::DrawInaccessibleTabWarning();
-        ImGui::Dummy(ImVec2(450, 0));
-        ImGui::End();
-        return;
+        ImGui::AlignTextToFramePadding();
+        ImGui::PushFont(UIManager::Get().GetBoldFont());
+        ImGui::Text(label);
+        ImGui::PopFont();
+        ImGui::SameLine();
+
+        const char* resetlabel = ICON_FA_REPEAT " Reset ";
+        const bool ret = ImGui::Button(resetlabel);
+        return ret;
     }
 
-    if (ImGui::Begin("Player Death Animations##2", &visible, ImGuiWindowFlags_NoCollapse))
+    void PlayerAnimation::Render()
     {
-        static std::int32_t selected = -1;
-        if (DrawHeaderAndResetButton(ICON_FA_PERSON_FALLING "  Select a death animation   "))
-            Components::PlayerAnimation::SetSelectedAnimIndex(selected = -1);
-        
-        const auto& anims = Components::PlayerAnimation::GetAnimations();
-        for (std::int32_t i = 0; i < std::ssize(anims); ++i)
-        {
-            assert(anims[i].first.length() > 0 && *(anims[i].first.data() + anims[i].first.length()) == '\0');
+        if (!visible)
+            return;
 
-            if (ImGui::Selectable(anims[i].first.data(), selected == i))
-                Components::PlayerAnimation::SetSelectedAnimIndex(selected = i);
+        ImGui::SetNextWindowFocus();
+
+        if (Mod::GetGameInterface()->GetGameState() != Types::GameState::InDemo)
+        {
+            ImGui::Begin("Player Death Animations##1", &visible, ImGuiWindowFlags_NoCollapse);
+            UI::DrawInaccessibleTabWarning();
+            ImGui::Dummy(ImVec2(450, 0));
+            ImGui::End();
+            return;
         }
 
-        ImGui::NewLine();
-
-        if (const auto animName = Components::PlayerAnimation::GetLatestAnimationName(); !animName.empty())
-            ImGui::Text("Latest death animation: \n%s", animName.data());
-        else 
+        if (ImGui::Begin("Player Death Animations##2", &visible, ImGuiWindowFlags_NoCollapse))
         {
-            ImGui::NewLine();
-            ImGui::NewLine();
-        }
+            static std::int32_t selected = -1;
+            if (DrawHeaderAndResetButton(ICON_FA_PERSON_FALLING "  Select a death animation   "))
+                Components::PlayerAnimation::SetSelectedAnimIndex(selected = -1);
         
-        ImGui::End();
+            const auto& anims = Components::PlayerAnimation::GetAnimations();
+            for (std::int32_t i = 0; i < std::ssize(anims); ++i)
+            {
+                assert(anims[i].first.length() > 0 && *(anims[i].first.data() + anims[i].first.length()) == '\0');
+
+                if (ImGui::Selectable(anims[i].first.data(), selected == i))
+                    Components::PlayerAnimation::SetSelectedAnimIndex(selected = i);
+            }
+
+            ImGui::NewLine();
+
+            if (const auto animName = Components::PlayerAnimation::GetLatestAnimationName(); !animName.empty())
+                ImGui::Text("Latest death animation: \n%s", animName.data());
+            else 
+            {
+                ImGui::NewLine();
+                ImGui::NewLine();
+            }
+        
+            ImGui::End();
+        }
     }
-}
 
-void PlayerAnimation::Release()
-{
-}
+    void PlayerAnimation::Release()
+    {
+    }
 
-void PlayerAnimation::Initialize()
-{
-}
+    void PlayerAnimation::Initialize()
+    {
+    }
 } // namespace IWXMVM::UI
