b12f6364d6ca5383ac0b059b47333efe8cbffdef
Caball <Caball009@al.al>
2024-06-25 22:57:46 +0200

    Added hide corpses boolean and setter/getter.

--- core/src/Components/PlayerAnimation.hpp
+++ core/src/Components/PlayerAnimation.hpp
@@ -8,5 +8,6 @@ namespace IWXMVM::Components::PlayerAnimation
     std::string_view GetLatestAnimationName();
     const std::vector<std::pair<std::string_view, std::uint32_t>>& GetAnimations();
     void SetSelectedAnimIndex(std::int32_t index);
+    bool& HideAllCorpses();
     bool& AttachWeaponToCorpse();
 }  // namespace IWXMVM::Components::PlayerAnimation
